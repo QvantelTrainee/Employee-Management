b81972976192a58b3a0c893bdf52eb5c7d74660f
Bharat Jain <bharat.jain@qvantel.com>
2023-02-13 13:02:14 +0530

    add employee model and make work with django admin

--- EmployeeManager/models.py
+++ EmployeeManager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dept_name = models.CharField(max_length=50)
+    salary = models.IntegerField(default=0)
+    bonus = models.IntegerField(default=0)
+    role = models.CharField(max_length=50)
+    hire_date = models.DateField()
+
+    def __str__(self):
+        return self.first_name
+    
+
 # Create your models here.
