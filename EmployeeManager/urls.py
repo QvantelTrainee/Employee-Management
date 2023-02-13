269001bf940ae8d8916fbbc502ce72edc4496f2b
Bharat Jain <bharat.jain@qvantel.com>
2023-02-13 22:45:57 +0530

    add Employee Details Page Backend

--- EmployeeManager/urls.py
+++ EmployeeManager/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    
-   ]
+   path('view_emp/<int:emp_id>', views.view_emp)
+]
--- EmployeeManager/views.py
+++ EmployeeManager/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from . models import Employee
+
+def view_emp(request, emp_id=0):
+    try:
+        emp = Employee.objects.get(id = emp_id)
+        context = {'emp': emp}
+        return HttpResponse(context)
+    except:
+        return HttpResponse('Error occured')    
+        
+        
 
 # Create your views here.
