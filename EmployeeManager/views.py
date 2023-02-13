185b6ffb96b538686532429536fafe2d663519db
Rashmi Sharma <rashmi.sharma@qvantel.com>
2023-02-13 17:12:48 +0530

    Corrections in static files

--- EmployeeManager/views.py
+++ EmployeeManager/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'Home.html' )
+def view(request):
+    return render(request, 'View_Employee.html' )
