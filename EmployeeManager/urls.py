9d0ac7ef095c7633628e45b2e2b1f834298e3ad5
Rashmi Sharma <rashmi.sharma@qvantel.com>
2023-02-14 15:58:59 +0530

    Commit

--- EmployeeManager/urls.py
+++ EmployeeManager/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('', views.index, name='index'),
 ]
-=======
+
    path('add_emp', views.add_emp, name='add_emp'),
    path('all_emp', views.all_emp, name='all_emp'),
    path('filter_emp', views.filter_emp, name='filter_emp'),
    ]
->>>>>>> 92feefcd5c21bf69d0c64030aa8f9bc8a0be4e50
+
